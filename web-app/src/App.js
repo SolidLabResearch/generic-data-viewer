b13c67ab4de6e3c22d22a0e6460fc02d4da26b3b
Buzogany Aron <aron.buzogany@ugent.be>
2023-06-26 14:59:14 +0200

    added base app architecture

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,9 +1,20 @@
 import ResultsTable from "./results-table/ResultsTable";
+import "./App.css"
+import SelectionTable from "./selection-table/SelectionTable";
 
 function App() {
   return (
     <div className="App">
-      <ResultsTable></ResultsTable>
+      <h1 className="app-title">Generic Data Viewer</h1>
+      <div className="app-body">
+        <SelectionTable />
+        <div className="right-field">
+          <ResultsTable/>
+        </div>
+      </div>
+      <footer>
+        
+      </footer>
     </div>
   );
 }
--- web-app/src/selection-table/SelectionTable.jsx
+++ web-app/src/selection-table/SelectionTable.jsx
@@ -1,9 +1,11 @@
-
+import "./SelectionTable.css"
 
 function SelectionTable(){
     return(
         <div className="selection-table">
-
+            <h2>Querries:</h2>
         </div>
     )
 }
+
+export default SelectionTable
