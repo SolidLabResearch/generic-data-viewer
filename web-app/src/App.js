d588006242cd08852b4fd2c617c54272232f2b4a
Buzogany Aron <aron.buzogany@ugent.be>
2023-06-26 17:28:40 +0200

    added config file with test values, added names to querry list

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,13 +1,20 @@
 import "./App.css"
 import SelectionTable from "./selection-table/SelectionTable";
 import RightField from "./right-field/RightField";
+import config from "./config.json" 
+
+config = JSON.parse(JSON.stringify(config))
+
+if(!config.querry_folder){
+  config.querry_folder = './'
+}
 
 function App() {
   return (
     <div className="App">
-      <h1 className="app-title">Generic Data Viewer</h1>
+      <h1 className="app-title">{config.title}</h1>
       <div className="app-body">
-        <SelectionTable />
+        <SelectionTable querries={config.querries}/>
         <RightField />
       </div>
       <footer><p><a href="https://idlab.technology/">IDLab</a> - <a href="https://www.imec.be/nl">imec</a> - <a href="https://www.ugent.be/">UGent</a></p></footer>
