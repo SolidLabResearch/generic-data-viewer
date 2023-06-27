3754038a088d9592c9849a230725f1d7607d97d6
Buzogany Aron <aron.buzogany@ugent.be>
2023-06-27 16:37:10 +0200

    added extendable querry selectors providing descriptions

---
+++ web-app/src/selection-table/SelectionTable.jsx
@@ -0,0 +1,21 @@
+import "./SelectionTable.css"
+import ExtendableSelector from "../components/ExtendableSelector"
+
+function SelectionTable(props){
+    const querries = props.querries
+    const onSelected = props.onSelected
+    return(
+        <div className="selection-table">
+            <h2 id="querries-title">Querries</h2>
+            <div className="selection-table-box">
+                <ul id="querry-list">
+                    {querries.map((element, index) => (
+                        <ExtendableSelector extendContent={element.description} key={index} className="querry-entry">
+                            <h3 onClick={() => onSelected(element)}>{element.name}</h3>
+                        </ExtendableSelector>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
