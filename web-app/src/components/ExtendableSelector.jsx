75e5e139d216fc123503bf7ff94291e73e7f1bb1
Buzogany Aron <aron.buzogany@ugent.be>
2023-06-30 09:16:28 +0200

    added jsdoc

--- web-app/src/components/ExtendableSelector.jsx
+++ web-app/src/components/ExtendableSelector.jsx
@@ -4,6 +4,12 @@ import { useState } from "react"
 // Arrow
 //https://stackoverflow.com/questions/30530946/collapse-arrow-on-mouse-click
 
+
+/**
+ * @param {EventListener} props.onClick an event listener for when the title in the component is clicked.
+ * @param {String} props.extendContent the content shown when the component is extended.  
+ * @returns A component with a title, which can be extended by clicking a button. If extended extra content is shown. 
+ */
 function ExtendableSelector({onClick, extendContent, children}){
     const [extended, setExtended] = useState(false)
     let switchExtended = () => setExtended((old) => {return !old})
