54fc672bf00b16548ca1af33b4f25f99e3dff206
abuzogan <aron.buzogany@ugent.be>
2023-07-17 16:08:22 +0200

    removed unused console log

--- src/right-field/results-table/ResultsTable.jsx
+++ src/right-field/results-table/ResultsTable.jsx
@@ -41,7 +41,6 @@ function ResultsTable(props){
     let adder = (item, variables) => setResults((old) => {
       let newValues = []
       for(let variable of variables){
-        console.log(variable)
         let value = item[variable] ? item[variable] : ""
         let type = variable.split('_')[1]
         let componentCaller = typeRepresentationMapper[type] 
@@ -102,7 +101,6 @@ function configureQueryWorker(adder, variableSetter, setIsQuerying){
       case 'result':
         let binding = JSON.parse(data.result)
         let entries = binding.entries 
-        console.log(entries)
         adder(entries, variablesMain)
           
         break; 
