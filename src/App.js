e3563506ecab0c88f63c4227a682fefa96e9a4bd
abuzogan <aron.buzogany@ugent.be>
2023-07-18 16:46:17 +0200

    chaged variable to camel case

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ config = JSON.parse(JSON.stringify(config))
  * @returns {Component} The root of the app.
  */
 function App() {
-  const [selectedquery, setSelectedquery] = useState(undefined)
+  const [selectedQuery, setSelectedQuery] = useState(undefined)
   return (
     <div className="App">
       <h1 className="app-title">{config.title}</h1>
       <div className="app-body">
-        <SelectionTable onSelected={(query) => setSelectedquery(query)} queries={config.queries}/>
-        <RightField query={selectedquery} />
+        <SelectionTable onSelected={(query) => setSelectedQuery(query)} queries={config.queries}/>
+        <RightField query={selectedQuery} />
       </div>
       <footer><p><a href="https://idlab.technology/">IDLab</a> - <a href="https://www.imec.be/nl">imec</a> - <a href="https://www.ugent.be/">UGent</a></p></footer>
 
