87e9008ba37640ba2d2ed44c1df538dc6a33bb16
abuzogan <aron.buzogany@ugent.be>
2023-07-19 14:28:10 +0200

    formated file

--- src/components/ExtendableSelector.jsx
+++ src/components/ExtendableSelector.jsx
@@ -1,30 +1,35 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 // Arrow
 //https://stackoverflow.com/questions/30530946/collapse-arrow-on-mouse-click
 
-
 /**
- * @param {String} props.extendContent the content shown when the component is extended.  
- * @returns A component with a title, which can be extended by clicking a button. If extended extra content is shown. 
+ * @param {String} props.extendContent the content shown when the component is extended.
+ * @returns A component with a title, which can be extended by clicking a button. If extended extra content is shown.
  */
-function ExtendableSelector({extendContent, children}){
-    const [extended, setExtended] = useState(false)
-    let switchExtended = () => setExtended((old) => {return !old})
-    return(
-        <>
-            <li className="query-entry">
-                <div className="extend-container">
-                     {children}
-                     <div onClick={(e) => {switchExtended(); e.target.classList.toggle('down')}} className="arrow"/>
-                </div>
-                <p className="extended-content">{extended && extendContent}</p>
-
-            </li>
-        </>
-    )
+function ExtendableSelector({ extendContent, children }) {
+  const [extended, setExtended] = useState(false);
+  let switchExtended = () =>
+    setExtended((old) => {
+      return !old;
+    });
+  return (
+    <>
+      <li className="query-entry">
+        <div className="extend-container">
+          {children}
+          <div
+            onClick={(e) => {
+              switchExtended();
+              e.target.classList.toggle("down");
+            }}
+            className="arrow"
+          />
+        </div>
+        <p className="extended-content">{extended && extendContent}</p>
+      </li>
+    </>
+  );
 }
 
-
-export default ExtendableSelector
+export default ExtendableSelector;
