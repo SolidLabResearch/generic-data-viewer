8e92c74d565a0019e97a6280c783ae94a8112a9a
abuzogan <aron.buzogany@ugent.be>
2023-07-19 11:31:13 +0200

    moved query logic from src/right-field/results-table/ResultsTable.jsx to src/right-field/RightField.jsx

--- src/right-field/RightField.jsx
+++ src/right-field/RightField.jsx
@@ -1,25 +1,163 @@
-import { useRef } from "react"
-import ResultsTable from "./results-table/ResultsTable"
-import "./RightField.css"
+import { useRef, useState, useEffect } from "react";
+import ResultsTable from "./results-table/ResultsTable";
+import "./RightField.css";
+import QueryWorker from "worker-loader!../workers/worker"
+import { typeRepresentationMapper } from '../typeMapper.js'
+import config from "../config.json"
+import Time from "../components/Time";
 
-/**
- * 
- * @param {query} props.query The query (as defined in the config file) that should be executed and results displayed in the table. 
- * @returns {Component} A React component that displays the given query in a table, with the functionality to refresh the results and to login for additional authorization.  
- */
-function RightField(props){
-    let query = props.query
-    let refreshRef = useRef(undefined) 
-    return(
-        <div className="right-field">
-            <div className="control-section">
-                <button ref={refreshRef}  disabled={query===undefined} id="refresh-button">Refresh</button>
-                {query && <label id="query-name-label">{query.name}</label>}
-                <button id="login-button">Login</button>
-            </div>
-            <ResultsTable refreshButton={refreshRef}  selectedQuery={query}/>
-        </div>
-    )
+
+if (!config.queryFolder) {
+  config.queryFolder = './'
 }
 
-export default RightField
+if (config.queryFolder.substring(config.queryFolder.length - 1) !== '/') {
+  config.queryFolder = `${config.queryFolder}/`
+}
+
+let queryWorker = undefined
+
+
+/**
+ *
+ * @param {query} props.query The query (as defined in the config file) that should be executed and results displayed in the table.
+ * @returns {Component} A React component that displays the given query in a table, with the functionality to refresh the results and to login for additional authorization.
+ */
+function RightField(props) {
+  let selectedQuery = props.query;
+  const [results, setResults] = useState([])
+  const [variables, setVariables] = useState([])
+  const [isQuerying, setQuerying] = useState(false)
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+    let intervalId
+    if(isQuerying){
+      intervalId = setInterval(() => setTime(time + 1), 10)
+    }
+    return () => clearInterval(intervalId)
+  }, [time, isQuerying])
+
+  useEffect(() => {
+    configureQueryWorker(adder, setVariables, setQuerying);
+  }, []);
+
+  let adder = adderFunctionMapper["bindings"](setResults);
+
+  const onQueryChanged = () => {
+    setTime(0)
+    if (selectedQuery) {
+      if (isQuerying) {
+        queryWorker.terminate();
+        configureQueryWorker(adder, setVariables, setQuerying);
+      }
+      setResults([]);
+      setVariables([]);
+      setQuerying(true);
+      executeQuery(selectedQuery, queryWorker);
+    }
+  };
+
+
+  useEffect(() => {
+    onQueryChanged();
+  }, [selectedQuery]);
+
+  return (
+    <div className="right-field">
+      <div className="control-section">
+        <button disabled={!selectedQuery} id="refresh-button" onClick={onQueryChanged}>
+          Refresh
+        </button>
+        <div id="query-information">
+          {selectedQuery && <label><strong>Result Count:</strong>{results.length}</label>}
+          {selectedQuery && <strong id="query-name-label">{selectedQuery.name}</strong>}
+          {selectedQuery && <label className="stopWatch"><strong >Runtime:</strong><Time time={time}/></label>}
+        </div>
+        <button id="login-button">Login</button>
+      </div>
+      <ResultsTable results={results} variables={variables} selectedQuery={selectedQuery} />
+    </div>
+  );
+}
+
+const adderFunctionMapper = {
+  bindings: (setter) => {
+    return (item, variable) => bindingStreamAdder(item, variable, setter);
+  },
+};
+
+/**
+ * Processes a result entry as it should look in the result table, and adds it to the table entries
+ * @param {Object} item the entry which should be processed
+ * @param {Array<String>} variables A list of all the variables in the table
+ * @param {Function} setter function to set the table entries.
+ */
+function bindingStreamAdder(item, variables, setter) {
+  let newValues = [];
+  for (let variable of variables) {
+    let value = item[variable] ? item[variable] : "";
+    let type = variable.split("_")[1];
+    let componentCaller = typeRepresentationMapper[type];
+    componentCaller = componentCaller ? componentCaller : (text) => text.value;
+    newValues.push(componentCaller(value));
+  }
+
+  setter((old) => {
+    return [...old, newValues];
+  });
+}
+
+/**
+ *
+ * @param {Function} adder function which processes the result, takes the result as argument
+ * @param {Function} variableSetter setter function for the variables, takes a list of variable names
+ * @param {Function} setIsQuerying boolean setter function for whether the worker is still executing a query or not
+ */
+function configureQueryWorker(adder, variableSetter, setIsQuerying) {
+  queryWorker = new QueryWorker();
+  let variablesMain = [];
+  queryWorker.onmessage = ({ data }) => {
+    switch (data.type) {
+      case "result":
+        let binding = JSON.parse(data.result);
+        let entries = binding.entries;
+        adder(entries, variablesMain);
+
+        break;
+      case "end":
+        setIsQuerying(false);
+        break;
+      case "metadata": {
+        variablesMain = data.metadata.variables.map((val) => val.value);
+        variableSetter(variablesMain);
+        break;
+      }
+    }
+  };
+}
+
+/**
+ * A function that executes a given query and processes every result as a stream based on the functions provided.
+ * @param {query} query the query which gets executed
+ * @param {Worker} queryWorker the worker which will be used to execute the given query
+ */
+async function executeQuery(query, queryWorker) {
+  try {
+    let result = await fetch(`${config.queryFolder}${query.queryLocation}`);
+    query.queryText = await result.text();
+    queryWorker.postMessage({ selectedQuery: query });
+  } catch (error) {
+    handleQueryFetchFail(error);
+  }
+}
+
+/**
+ * Handles the event whenever the fetching of a query fails.
+ * @param {Error} error the object returned by the fetch API whenever the fetch fails.
+ */
+function handleQueryFetchFail(error) {
+  console.error(error);
+}
+
+export default RightField;
