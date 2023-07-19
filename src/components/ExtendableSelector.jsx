b376c75df3b71f9d0a7262608a27cc2d7159e205
abuzogan <aron.buzogany@ugent.be>
2023-07-19 14:27:38 +0200

    added additional documentation

--- src/components/ExtendableSelector.jsx
+++ src/components/ExtendableSelector.jsx
@@ -6,11 +6,10 @@ import { useState } from "react"
 
 
 /**
- * @param {EventListener} props.onClick an event listener for when the title in the component is clicked.
  * @param {String} props.extendContent the content shown when the component is extended.  
  * @returns A component with a title, which can be extended by clicking a button. If extended extra content is shown. 
  */
-function ExtendableSelector({onClick, extendContent, children}){
+function ExtendableSelector({extendContent, children}){
     const [extended, setExtended] = useState(false)
     let switchExtended = () => setExtended((old) => {return !old})
     return(
