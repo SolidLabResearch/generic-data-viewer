05d6f8ee0ef6ae6624bfce17ff4747db9f6f612d
abuzogan <aron.buzogany@ugent.be>
2023-07-26 16:24:00 +0200

    added additional test

--- src/__tests__/ResultsTable.test.jsx
+++ src/__tests__/ResultsTable.test.jsx
@@ -4,21 +4,27 @@ import ResultsTable from '../right-field/results-table/ResultsTable'
 
 
 test('Load with a label mentioning no query is yet selected.', () => {
-    const {container} = render(<ResultsTable></ResultsTable>)
-    let parentDiv = container.firstChild
-    let noQueryLabel = screen.queryByTestId('no-query-label')
+    const {container} = render(<ResultsTable variables={[]}></ResultsTable>)
+    const parentDiv = container.firstChild
+    const noQueryLabel = screen.queryByTestId('no-query-label')
     expect(parentDiv.firstChild).toEqual(parentDiv.lastChild)
     expect(noQueryLabel).toBeInTheDocument()
 })
 
 test('Label mentioning no query is selected is not present if query is selected', () => {
     render(<ResultsTable variables={["Test1"]} results={["testData"]} selectedQuery={{name: "Test query"}}></ResultsTable>)
-    let noQueryLabel = screen.queryByTestId('no-query-label')
+    const noQueryLabel = screen.queryByTestId('no-query-label')
     expect(noQueryLabel).not.toBeInTheDocument()
 })
 
 test('A query with variables exposes the gridjs table', () => {
     const {container} = render(<ResultsTable variables={["Test1"]} results={["testData"]} selectedQuery={{name: "Test query"}}></ResultsTable>)
-    let gridJsTable = container.getElementsByClassName('gridjs').item(0)
+    const gridJsTable = container.getElementsByClassName('gridjs').item(0)
     expect(gridJsTable).not.toBeUndefined()  
+})
+
+test('The GridJS table isn\'t present unless there are variables', () => {
+    const {container} = render(<ResultsTable variables={[]} results={["testData"]} selectedQuery={{name: "Test query"}}></ResultsTable>)
+    const gridJsTable = container.getElementsByClassName('gridjs').item(0)
+    expect(gridJsTable).toBeNull()
 })
