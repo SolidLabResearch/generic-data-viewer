2fc403b2573694557795292567d866159887364c
Buzogany Aron <aron.buzogany@ugent.be>
2023-08-01 09:25:10 +0200

    made columns resizable

--- src/right-field/results-table/ResultsTable.jsx
+++ src/right-field/results-table/ResultsTable.jsx
@@ -24,12 +24,13 @@ function ResultsTable(props) {
     <div className="results-table" style={{marginTop: 0}}>
       {!selectedQuery && <label data-testid="no-query-label">Please select a query.</label>}
       {variables.length > 0 &&
-        <Grid style={{ th: { "height": "fit-content" }, container: { "margin": "0" }, table: {"overflow": "hidden"} }}
+        <Grid style={{ th: { "height": "fit-content" }, container: { "margin": "0"}, table: {"overflow": "hidden"} }}
           className={{ tbody: "grid-body" }}
           data-testid="gridjs"
           data={results}
           sort={true}
           autoWidth={!props.isQuerying}
+          resizable={!props.isQuerying}
           ref={containerRef}
           columns={variables.map(column => { return generateColumn(column, variables.length, props.isQuerying) })} />
       }
@@ -42,13 +43,17 @@ function ResultsTable(props) {
  * @param {String} variable a variable name 
  * @returns {Object} a configuration object corresponding to a column, following GridJS config. 
  */
-function generateColumn(variable) {
+function generateColumn(variable, size, isQuerying) {
   let variableSplitted = variable.split('_')
   let config = {
     name: variableSplitted[0],
+    minWidth: "1px" ,
     sort: {
       compare: typeSortMapper[variableSplitted[1]]
     }
+  }
+  if(!isQuerying) {
+    config.width = `${100 / size}%`
   }
   return config
 }
