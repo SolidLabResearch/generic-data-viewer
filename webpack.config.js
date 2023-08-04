c5490f902aaa827a0396b12b91a263f2ea6d9d03
Buzogany Aron <aron.buzogany@ugent.be>
2023-08-04 10:42:02 +0200

    removed react-scripts from dependencies and made own webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "production",
+  entry: "./src/index.js",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        resolve: {
+            fullySpecified: false,
+        },
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  target: ["web", "es5"],
+};
